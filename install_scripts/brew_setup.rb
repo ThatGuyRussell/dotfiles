2074ce5a3b84fba72f9524722c756dbe40f77ba3
Russell Hawkins <russell.aspirations@gmail.com>
2022-02-12 15:06:31 -0500

    Organize brew setup

--- install_scripts/brew_setup.rb
+++ install_scripts/brew_setup.rb
@@ -1,23 +1,28 @@
 require "#{File.dirname __FILE__}/user_ask"
 
 def brew_installed?
-  `which brew &> /dev/null`
+  `command -v brew &> /dev/null`
   $?.success?
 end
 
 def install_brew()
   unless brew_installed?
-    unless user_ask :default_confirm, "Would you like to install homebrew?"
+    if user_ask :default_confirm, "Would you like to install homebrew?"
+      `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)"`
+      `export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH`
+      puts 'Homebrew was installed.'.green
+    else
       puts "Homebrew was not installed."
       return
     end
-    # Download and install homebrew
-    `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
-    puts 'Homebrew was installed.'.green
   else
     puts 'Homebrew is already installed.'.green
   end
   puts
-  puts 'Run `brew bundle --global` to install default packages.'.yellow
+  if user_ask :default_confirm, 'Would you like to install your Homebrew default packages now?'
+    `brew bundle --global`
+  else
+    puts 'Run `brew bundle --global` when you want to install default packages.'.yellow
+  end
   puts '(Any cask apps that are already installed without cask must first be uninstalled)'.yellow
 end
